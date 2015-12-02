00e2e40ccb597900bf4f942c71522ba9fb1003d5
DannyNicholas <dannynicholas23@googlemail.com>
2015-12-02 14:55:50 +0000

    Initial Commit.

---
+++ spring-ui/src/test/java/com/danosoftware/spring/web/HomeControllerTest.java
@@ -0,0 +1,17 @@
+package com.danosoftware.spring.web;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HomeControllerTest
+{
+
+    @Test
+    public void testHomePage() throws Exception
+    {
+        HomeController controller = new HomeController();
+        assertEquals("home", controller.home());
+    }
+
+}
